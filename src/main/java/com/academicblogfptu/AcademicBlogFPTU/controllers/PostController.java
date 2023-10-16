d32213499c3f7f3b14dc21a183e0f4889ad2cae6
TuNguyen123vn <nguyentu100312@gmail.com>
2023-10-16 23:38:28 +0700

    Merge remote-tracking branch 'origin/TuNguyenBranch' into TuNguyenBranch

--- src/main/java/com/academicblogfptu/AcademicBlogFPTU/controllers/PostController.java
+++ src/main/java/com/academicblogfptu/AcademicBlogFPTU/controllers/PostController.java
@@ -1,29 +1,46 @@
 package com.academicblogfptu.AcademicBlogFPTU.controllers;
 
-import com.academicblogfptu.AcademicBlogFPTU.dtos.PostDto;
+import com.academicblogfptu.AcademicBlogFPTU.dtos.*;
+import com.academicblogfptu.AcademicBlogFPTU.entities.PostEntity;
 import com.academicblogfptu.AcademicBlogFPTU.services.PostServices;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
 public class PostController {
 
     @Autowired
     private final PostServices postServices;
 
-    @GetMapping("/view-post")
-    public ResponseEntity<List<PostDto>> getPostList(){
-        List<PostDto> list = postServices.viewAllPost();
+    @GetMapping("users/post-list")
+    public ResponseEntity<List<PostListDto>> getPostList(){
+        List<PostListDto> list = postServices.viewAllPost();
         return ResponseEntity.ok(list);
     }
 
+    @PostMapping("users/view-post")
+    public ResponseEntity<PostDto> viewAPost(@RequestBody PostDto postId){
+        PostDto post = postServices.viewPostById(postId.getPostId());
+        return ResponseEntity.ok(post);
+    }
 
+    @PostMapping("posts/delete")
+    public ResponseEntity<HttpStatus> deletePostById(@RequestBody PostDto postId){
+        postServices.deletePostById(postId.getPostId());
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PostMapping("users/request-post")
+    public ResponseEntity<PostDto> requestPost(@RequestBody RequestPostDto requestPostDto){
+        PostDto newPost = postServices.requestPost(requestPostDto);
+        postServices.postDetail(newPost.getPostId());
+        return ResponseEntity.ok(newPost);
+    }
 }
